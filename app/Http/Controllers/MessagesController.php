0cc30ae8dc840871d69c8f5c887ba909c84a1cda
anayarojo <raulmartin_04@hotmail.com>
2018-06-26 12:35:13 -0700

    Factories and database seed

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Message;
+use App\Http\Requests\CreateMessageRequest;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
@@ -14,10 +15,13 @@ class MessagesController extends Controller
         ]);
     }
 
-    public function create(Request $request){
-        $this->validate($request, [
-            "message" => ["required", "max:16"]
+    public function create(CreateMessageRequest $request){
+
+        $message = Message::create([
+            "content" => $request->input("message"),
+            "image" => "http://lorempixel.com/600/338?".mt_rand(0,10000)
         ]);
-        return "Created";
+
+        return redirect("/messages/".$message->id);
     }
 }
---
+++ app/Http/Requests/CreateMessageRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "message" => ["required", "max:16"]
+        ];
+    }
+    
+    public function messages(){
+
+        return[
+            "message.required" => "Por favor escribe tu mensaje.",
+            "message.max" => "Por favor escribe un mensaje no mayor a 16 caracteres."
+        ];
+    }
+}
