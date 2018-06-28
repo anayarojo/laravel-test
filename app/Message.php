d6b06313175e392b8bffaf11c334db13f717b0b6
anayarojo <raulmartin_04@hotmail.com>
2018-06-28 15:00:51 -0700

    Search with algolia

--- app/Message.php
+++ app/Message.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Message extends Model
 {
+    use Searchable;
+
     protected $guarded = [];
 
     public function user()
@@ -18,7 +21,13 @@ class Message extends Model
         if (!$image || starts_with($image, "http")) {
             return $image;
         }
-        
+
         return \Storage::disk("public")->url($image);
+    }
+
+    public function toSearchableArray()
+    {
+        $this->load("user");
+        return $this->toArray();
     }
 }
